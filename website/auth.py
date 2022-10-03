8d59a00345240060c2a5d85fccc1c9355ab391cb
WraitH17 <saranshsinha084@gmail.com>
2022-10-03 13:14:57 +0530

    first-commit

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint,render_template
+
+auth= Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+@auth.route('/sign-up')
+def signUp():
+    return render_template('signUp.html')
+
